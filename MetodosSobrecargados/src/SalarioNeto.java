a228a3bd98b6e2b1e4a5dc2fa5f1b55e0475ffab
Danilo Noguera <danilo.noguera@gmail.com>
2023-10-03 17:46:46 -0600

    Otras propuestas de metodos para el ejercicio del calculo del salario neto

---
+++ MetodosSobrecargados/src/SalarioNeto.java
@@ -0,0 +1,20 @@
+public class SalarioNeto {
+    public static void main(String[] args) {
+        //Datos de entrada
+        //tasas son constantes o variables
+        //el usuario puede ingresar solo su salario bruto
+        //pero tambien se le podria pedir las tasas del inss, ir, inss patronal, antiguedad, sindicato
+        
+    }
+    //Propuestas de metodos
+        //pedirle el salario bruto al usuario
+        //calcular ingreso total
+        //calcular ingresos por antiguedad
+        //calcular deducciones totales
+        //calcular monto del inss
+        //calcular monto del ir
+        //calcular monto por afiliacion al sindicato
+        //calcular salario neto
+        //calcular inss patronal
+        //mostrar los resultados detallados de cada calculo
+}
